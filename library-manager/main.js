a6655f5942f214d65847cd632f809aa5cb67b338
oginoshikibu <oginoshikibu@gmail.com>
2023-03-07 04:37:00 +0900

    scrapbox部分調査開始

--- library-manager/main.js
+++ library-manager/main.js
@@ -1,224 +1,179 @@
-javascript:
-alert("ok");
+javascript: (function () {
+	var D = document, B = D.getElementsByTagName(`body`)[0];
+	function bmlPanel(id, w, h) {
+		var P, F, H, X, DP, CL;
+		B.appendChild(P = D.createElement(`div`));
+		P.id = id;
+		P.style.cssText = ` position:fixed;
+		top:10px;
+		left:10px;
+		padding:2px;
+		width:`+ w + `px;
+		height:`+ h + `px;
+		opacity:0.8;
+		filter:alpha(opacity = 80);
+		background:#000;
+		border:1px solid #666;
+		color:#fff;
+		font-size:12px;
+		text-align:left;
+		z-index:9998;
+		-moz-border-radius:5px;`;
+		P.appendChild(H = D.createElement(`div`));
+		H.innerHTML = id;
+		H.style.cssText = ` padding:0px 10px;
+		height:20px;
+		line-height:20px;
+		color:#fff;
+		font-size:12px;
+		font-weight:bold;
+		text-align:center;
+		cursor:move;`;
+		DP = D.createElement(`div`);
+		DP.style.cssText = `background:transparent;
+		position:fixed;
+		top:0px;
+		left:0px;
+		width:100%;
+		height:100%;`;
+		P.appendChild(F = D.createElement(`div`));
+		with (F.style) {
+			height = h - 20 + `px`;
+			overflow = `auto`;
+			backgroundColor = `#222`;
+			color = `#eee`;
+			fontSize = `11px`;
+			cursor = `auto`;
+		}
+		P.appendChild(CL = D.createElement(`div`));
+		CL.title = `close`;
+		with (CL.style) {
+			border = `#444 solid 1px`;
+			position = `absolute`;
+			top = "5px";
+			left = "5px";
+			height = `10px`;
+			width = `10px`;
+			cursor = `pointer`;
+		}
+		CL.onclick = function () {
+			B.removeChild(this.parentNode);
+		}
+			;
+		H.onmousedown = function (e) {
+			x = (e) ? e.pageX : event.x;
+			y = (e) ? e.pageY : event.y;
+			ox = x - P.offsetLeft;
+			oy = y - P.offsetTop;
+			P.appendChild(DP);
+			return false;
+		}
+			;
+		DP.onmousemove = function (e) {
+			x = (e) ? e.pageX : event.x;
+			y = (e) ? e.pageY : event.y;
+			P.style.left = (x - ox) + `px`;
+			P.style.top = (y - oy) + `px`;
+			return false;
+		}
+			;
+		DP.onmouseup = function () {
+			P.removeChild(DP);
+			return false;
+		}
+			;
+		P.header = H;
+		P.content = F;
+		return P;
+	}
 
-
-
-function getScrollPosition() {
-    return (document.documentElement.scrollTop || document.body.scrollTop);
-}
-
-
-
-function eventMouseup() {
-    if (intPanelMover == 0) {
-        sText = selectedText();
-        if (sText) {
-            document.getElementById(`txtSearch`).value = sText;
-        }
-    }
-}
-
-function eventMover() {
-    intPanelMover = 1;
-}
-function eventMout() {
-    intPanelMover = 0;
-}
-
-
-
-function panel_close() {
-    document.getElementById(`popSearchPnl2013`).style.visibility = "hidden";
-    document.getElementById(`popSearchPnl2013`).style.display = "none";
-    document.getElementById(`txtSearch`).value = ``;
-}
-
-function panel_open() {
-    sYPosition = getScrollPosition() + 20;
-    document.getElementById(`popSearchPnl2013`).style.top = sYPosition + "px";
-    document.getElementById(`popSearchPnl2013`).style.visibility = "visible";
-    document.getElementById(`popSearchPnl2013`).style.display = "block";
-}
-
-function displayPanel() {
-
-    var sText = selectedText();
-
-    var oPopSearch = document.getElementById(`popSearchPnl2013`);
-
-    if (oPopSearch) {
-        panel_open();
-    } else {
-        var o = document.createElement("div");
-        sPanel =
-            `<div id="popSearchPnl2013" onmouseover="eventMover()" onmouseout="eventMout()">` +
-            `<div class="paneltitle" id="paneltitle"><span>書籍横断検索システム 検索パネル</span></div>` +
-            `<form name="main_form" id="main_form" action="http:book.tsuhankensaku.com/cgi-bin/panelsearch.cgi" target="_self" method="post">` +
-            `<input type="text" name="ws" value="" class="txtSearch" id="txtSearch" maxlength="256" autocomplete="off"><input type="hidden" name="cc" value="0" id="typeCharSet">` +
-            `<div id="btnpanel"><input name="book-shosekioudan" id="shosekioudan" type="submit" value="「書籍横断検索システム」で検索" style="width:232px;border:1px solid #666;border-top:0px;">` +
-            `<input name="robo-google" type="submit" value="Google" id="item-yahoo"><input name="item-rakuten" type="submit" value="楽天" id="item-rakuten"><input name="item-amazon" type="submit" value="Amazon" id="item-amazon">` +
-            `<input name="pric-kakaku" type="submit" value="価格コム" id="pric-kakaku" style="border-right:1px solid #666;">` +
-            `<div style="float:left;"><a class="panelclose" href="javascript:panel_close();" title="パネルを閉じます。ESCキーでパネルを閉じることもできます。">パネルを閉じる</a></div>` +
-            `<div style="float:right;margin:3px 10px 0px 0px;">` +
-            `<a class="homeicon" href="http:book.tsuhankensaku.com/" title="本・書籍通販検索へ移動">本</a>` +
-            `<a class="homeicon" href="http:book.tsuhankensaku.com/hon/" title="書籍横断検索システムへ移動">書</a>` +
-            `</div>` +
-            `</form>` +
-            `</div>` +
-            `</div>`;
-        o.innerHTML = sPanel;
-        document.body.appendChild(o);
-        createCss();
-        document.getElementById(`txtSearch`).value = sText;
-        document.getElementById(`typeCharSet`).value = "utf-8";
-        sYPosition = getScrollPosition() + 20;
-        document.getElementById(`popSearchPnl2013`).style.top = sYPosition + "px";
-        if (sIEdocmode == 7) {
-            document.getElementById(`popSearchPnl2013`).style.width = "241px";
-            document.getElementById(`txtSearch`).focus();
-            document.getElementById(`txtSearch`).blur();
-        } else if (sIEdocmode == 5 || sIEdocmode == 6) {
-            document.getElementById(`popSearchPnl2013`).style.width = "252px";
-            document.getElementById(`txtSearch`).style.width = "232px";
-            document.getElementById(`txtSearch`).focus();
-            document.getElementById(`txtSearch`).blur();
-            for (i = 2; i <= 6; i = i + 1) {
-                document.getElementById(`popSearchPnl2013`).getElementsByTagName(`input`).item(i).style.backgroundColor = "#e4bC00";
+	var t = `` + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text);
+	if (!t) {
+		L = document.location.href;
+		if (L.match(/(yahoo\.).+?.*p=([^&]+)/) || L.match(/(amazon\.).+?.*field-keywords=([^&]+)/) || L.match(/(\.wikipedia\.).+\/wiki\/([^\/]+)/) || L.match(/(youtube\.).+search_query=([^&]+)/) || L.match(/(\?.*\b)q=([^&]+)/) || L.match(/(\#search\/)([^\/]+)/)) t = decodeURIComponent(RegExp.$2);
+	}
+	Es = [[`Amazon`, `http://www.amazon.co.jp/exec/obidos/external-search/?keyword=%%`]];
+	var SP = bmlPanel(`sp`, 200);
+	SP.header.innerHTML = `検索パネル`;
+	var C = SP.content, qt, tc = [], td = [], t, i, sbm, tmp;
+	C.appendChild(qt = D.createElement(`input`));
+	qt.value = t;
+	qt.style.cssText = ` display:block;
+	width:90%;
+	margin:6px;
+	border:1px solid #666;
+	background:transparent;
+	color:inherit;
+	font-weight:bold;
+	font-size:13px;`;
+	for (i = 0; i < Es.length; i++) {
+		C.appendChild(tc[i] = D.createElement(`input`));
+		C.appendChild(td[i] = D.createElement(`div`));
+		C.appendChild(tmp = D.createElement(`div`));
+		td[i].url = Es[i][1];
+		tmp.style.cssText = `clear:both`;
+		with (tc[i]) {
+			type = `checkbox`;
+			name = i;
+			value = Es[i][1];
+			style.cssText = ` float:left;
+			clear:left;
+			display:block;
+			margin:2px 6px;`;
+		}
+		with (td[i]) {
+			style.cssText = ` cursor:pointer;`;
+			onmouseover = function () {
+				with (this.style) {
+					fontWeight = `bold`;
+					color = `#ff8`
+				}
+			}
+				;
+			onmouseout = function () {
+				with (this.style) {
+					fontWeight = `inherit`;
+					color = `inherit`
+				}
+			}
+				;
+			onclick = function () {
+				open(this.url.replace(/%%/, qt.value), `_blank`);
+			}
+				;
+			innerHTML = Es[i][0];
+		}
+	}
+	C.appendChild(sbm = D.createElement(`button`));
+	with (sbm) {
+		innerHTML = `チェックしたサービスで一括検索`;
+		style.cssText = ` cursor:pointer;
+		margin:6px auto;
+		display:block;
+		clear:both;`;
+		onclick = function () {
+            var p = document.getElementById("productTitle");
+            if (!p) var p = document.getElementById("ebooksProductTitle");
+            var title = window.prompt(`Scrap "Amazon" to your scrapbox.`, p.innerHTML);
+            if (!title) return;
+            title = `『` + title + `』`;
+            var imagecontainer = document.getElementById("imageBlockContainer");
+            if (!imagecontainer) var imagecontainer = document.getElementById("ebooksImageBlockContainer");
+            var images = imagecontainer.getElementsByTagName("img");
+            var image = images[images.length-1];
+            var imageurl = image.getAttribute("src");
+            var pub = [];
+            var c = document.getElementsByClassName(`author`);
+            for (g = 0; g < c.length; g++) {
+                let at = c[g].innerText.replace(/,/, ``);
+                let pu = at.match(/\(.+\)/);
+                let ct = at.replace(/\(.+\)/, ``).replace(/ /g, ``);
+                pub.push(pu + ` [` + ct + `]`);
             }
+            var lines = `[` + imageurl + ` ` + window.location.href + `]\n` + pub.join(` `) + `\n#本`;
+            var body = encodeURIComponent(lines);
+            window.open(`https://scrapbox.io/oginos-reading-record/` + encodeURIComponent(title.trim()) + `?body=` + body)
         }
-    }
+	}
 }
-
-
-function createCss() {
-    var eWrapper = document.createElement(`div`);
-    sCss =
-        `#popSearchPnl2013,#popSearchPnl2013 form, #popSearchPnl2013 input, #popSearchPnl2013 #txtSearch, #popSearchPnl2013 .panelclose, #popSearchPnl2013 .homeicon {` +
-        `font-family: "メイリオ","Meiryo","ＭＳ Ｐゴシック", "MS P Gothic","Osaka",Verdana,Arial, Helvetica, sans-serif !important;` +
-        `font-size:12px !important;` +
-        `margin:0px !important;` +
-        `}` +
-        `#popSearchPnl2013 {` +
-        `width:240px;` +
-        `padding:10px 0px 5px 8px;` +
-        `text-align:left;` +
-        `position:absolute;top:10px; left:10px;` +
-        `z-index:2147483647;` +
-        `background-color: #aaa;` +
-        `font-size:12px;` +
-        `border:2px solid #ccc;` +
-        `box-shadow: 1px 1px 3px #000;` +
-        `-moz-box-shadow: 1px 1px 3px #000;` +
-        `-webkit-box-shadow: 1px 1px 3px #000;` +
-        `filter:progid:DXImageTransform.Microsoft.Shadow(Color=#000000, Strength=2, Direction=135);` +
-        `-webkit-border-radius: 5px;` +
-        `-moz-border-radius: 5px;` +
-        `border-radius: 5px;` +
-        `}` +
-        `#popSearchPnl2013 #btnpanel input {` +
-        `width:58px;` +
-        `height:26px;` +
-        `font-size:12px;` +
-        `vertical-align:text-bottom;` +
-        `box-shadow: 1px 1px 1px #999;` +
-        `-moz-box-shadow: 1px 1px 1px #999;` +
-        `-webkit-box-shadow: 1px 1px 1px #999;` +
-        `padding:0px;` +
-        `margin:0px 0px 0px 0px;` +
-        `border-style: solid;` +
-        `border-width: 0px 0px 1px 1px;` +
-        `border-color: #666;` +
-        `color: #000;` +
-        `text-decoration: none;` +
-        `text-align: center;` +
-        `background-color:#e4bC00;` +
-        `filter:progid:DXImageTransform.Microsoft.Gradient(GradientType=0,StartColorStr=#F4CC0B,EndColorStr=#e4bC00);` +
-        `background: -moz-linear-gradient(top,#F4CC0B 0%,#e4bC00);` +
-        `background: -webkit-gradient(linear, left top, left bottom, from(#F4CC0B), to(#e4bC00));` +
-
-        `}` +
-        `#popSearchPnl2013 #btnpanel input:hover {` +
-        `filter:progid:DXImageTransform.Microsoft.Gradient(GradientType=0,StartColorStr=#ffffee,EndColorStr=#ffffee);` +
-        `background:#ffc;` +
-        `cursor:pointer;` +
-        `}` +
-        `#popSearchPnl2013 .paneltitle {` +
-        `width:232px;` +
-        `height:23px;` +
-        `background-color:#666;` +
-        `margin:0px;` +
-        `border:0px solid #444;` +
-        `padding:4px 0px 0px 0px;` +
-        `color:#fff;` +
-        `font-weight:bold;` +
-        `filter:progid:DXImageTransform.Microsoft.Gradient(GradientType=0,StartColorStr=#666666,EndColorStr=#222222);` +
-        `background: -moz-linear-gradient(top,#666666 0%,#222222);` +
-        `background: -webkit-gradient(linear, left top, left bottom, from(#666666), to(#222222));` +
-        `}` +
-        `#popSearchPnl2013 .paneltitle span {` +
-        `color:#fff;` +
-        `text-decoration:none;` +
-        `background-color:transparent;` +
-        `padding:2px 4px 1px 4px;` +
-        `margin:0px 0px 0px 4px;` +
-        `-webkit-border-radius: 5px;` +
-        `-moz-border-radius: 5px;` +
-        `border-radius: 5px;` +
-        `}` +
-        `#popSearchPnl2013 .panelclose {` +
-        `cursor:pointer;` +
-        `width:92px;` +
-        `border-bottom:1px solid #333;` +
-        `margin-top:2px;` +
-        `display:block;` +
-        `margin-top:4px !important;` +
-        `padding:2px 4px 1px 4px;` +
-        `color:#fff;` +
-        `background-color:#666;` +
-        `font-weight:normal;` +
-        `text-decoration:none;` +
-        `-webkit-border-radius: 5px;` +
-        `-moz-border-radius: 5px;` +
-        `border-radius: 5px;` +
-        `}` +
-        `#popSearchPnl2013 .homeicon {` +
-        `cursor:pointer;` +
-        `border-bottom:1px solid #333;` +
-        `display:inline-block;` +
-        `margin:0px 0px 0px 2px !important;` +
-        `padding:2px 4px 1px 4px !important;` +
-        `color:#fff;` +
-        `background-color:#666;` +
-        `font-weight:normal;` +
-        `text-decoration:none;` +
-        `-webkit-border-radius: 5px;` +
-        `-moz-border-radius: 5px;` +
-        `border-radius: 5px;` +
-        `}` +
-        `#popSearchPnl2013 .panelclose:hover, #popSearchPnl2013 .homeicon:hover {` +
-        `color:#000;` +
-        `background:#ffc;` +
-        `cursor:pointer;` +
-        `}` +
-        `#popSearchPnl2013 #txtSearch {` +
-        `width:230px;` +
-        `height:22px;`
-
-    if (sIEdocmode == 5 || sIEdocmode == 6) {
-        sCss += "margin:-1px 0px !important;";
-    }
-
-    sCss +=
-        `box-shadow: 0px 1px 3px 1px #ccc inset;` +
-        `font-size:14px;` +
-        `color:#000;` +
-        `padding:0px;` +
-        `border:1px solid #666;` +
-        `background:#fafafa;` +
-        `}`
-
-    eWrapper.innerHTML = `a<style type="text\/css">` + sCss + `<\/style>`;
-    document.getElementsByTagName(`head`)[0].appendChild(eWrapper.lastChild);
-}
-
-displayPanel();
+)()
