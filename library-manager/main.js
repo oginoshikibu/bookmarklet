da772efdf9f0289b4a71e08e886ea43ba1b6864c
oginoshikibu <oginoshikibu@gmail.com>
2023-03-09 04:33:09 +0900

    cut and refactoring

--- library-manager/main.js
+++ library-manager/main.js
@@ -1,17 +1,18 @@
 javascript: (function () {
-	var D = document, B = D.getElementsByTagName(`body`)[0];
-	function bmlPanel(id, w, h) {
-		var P, F, H, X, DP, CL;
-		B.appendChild(P = D.createElement(`div`));
-		P.id = id;
-		P.style.cssText = ` position:fixed;
+	var body = document.getElementsByTagName(`body`)[0];
+	function createPanel(panel_id, panel_width, panel_height) {
+		var panel, frame, header, display_panel, close_button;/*Q: display_panelの存在意義が不明*/
+
+		body.appendChild(panel = document.createElement(`div`));
+		panel.id = panel_id;
+		panel.style.cssText = ` position:fixed;
 		top:10px;
 		left:10px;
 		padding:2px;
-		width:`+ w + `px;
-		height:`+ h + `px;
+		width:`+ panel_width + `px;
+		height:`+ panel_height + `px;
 		opacity:0.8;
-		filter:alpha(opacity = 80);
+		filter:alpha(opacity = 95);
 		background:#000;
 		border:1px solid #666;
 		color:#fff;
@@ -19,9 +20,10 @@ javascript: (function () {
 		text-align:left;
 		z-index:9998;
 		-moz-border-radius:5px;`;
-		P.appendChild(H = D.createElement(`div`));
-		H.innerHTML = id;
-		H.style.cssText = ` padding:0px 10px;
+
+		panel.appendChild(header = document.createElement(`div`));
+		header.innerHTML = panel_id;
+		header.style.cssText = ` padding:0px 10px;
 		height:20px;
 		line-height:20px;
 		color:#fff;
@@ -29,25 +31,29 @@ javascript: (function () {
 		font-weight:bold;
 		text-align:center;
 		cursor:move;`;
-		DP = D.createElement(`div`);
-		DP.style.cssText = `background:transparent;
+
+		display_panel = document.createElement(`div`);
+		display_panel.style.cssText = `background:transparent;
 		position:fixed;
 		top:0px;
 		left:0px;
 		width:100%;
 		height:100%;`;
-		P.appendChild(F = D.createElement(`div`));
-		with (F.style) {
-			height = h - 20 + `px`;
+
+		panel.appendChild(frame = document.createElement(`div`));
+		frame.id = `frame`;
+		with (frame.style) {
+			height = panel_height - 20 + `px`;
 			overflow = `auto`;
 			backgroundColor = `#222`;
 			color = `#eee`;
 			fontSize = `11px`;
 			cursor = `auto`;
 		}
-		P.appendChild(CL = D.createElement(`div`));
-		CL.title = `close`;
-		with (CL.style) {
+
+		panel.appendChild(close_button = document.createElement(`div`));
+		close_button.title = `close button`;
+		with (close_button.style) {
 			border = `#444 solid 1px`;
 			position = `absolute`;
 			top = "5px";
@@ -56,49 +62,43 @@ javascript: (function () {
 			width = `10px`;
 			cursor = `pointer`;
 		}
-		CL.onclick = function () {
-			B.removeChild(this.parentNode);
+		close_button.onclick = function () {
+			body.removeChild(this.parentNode);
 		}
 			;
-		H.onmousedown = function (e) {
+		header.onmousedown = function (e) {
 			x = (e) ? e.pageX : event.x;
 			y = (e) ? e.pageY : event.y;
-			ox = x - P.offsetLeft;
-			oy = y - P.offsetTop;
-			P.appendChild(DP);
+			ox = x - panel.offsetLeft;
+			oy = y - panel.offsetTop;
+			panel.appendChild(display_panel);
 			return false;
 		}
 			;
-		DP.onmousemove = function (e) {
+		display_panel.onmousemove = function (e) {
 			x = (e) ? e.pageX : event.x;
 			y = (e) ? e.pageY : event.y;
-			P.style.left = (x - ox) + `px`;
-			P.style.top = (y - oy) + `px`;
+			panel.style.left = (x - ox) + `px`;
+			panel.style.top = (y - oy) + `px`;
 			return false;
 		}
 			;
-		DP.onmouseup = function () {
-			P.removeChild(DP);
+		display_panel.onmouseup = function () {
+			panel.removeChild(display_panel);
 			return false;
 		}
 			;
-		P.header = H;
-		P.content = F;
-		return P;
+		panel.header = header;
+		panel.content = frame;
+		return panel;
 	}
 
-	var t = `` + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text);
-	if (!t) {
-		L = document.location.href;
-		if (L.match(/(yahoo\.).+?.*p=([^&]+)/) || L.match(/(amazon\.).+?.*field-keywords=([^&]+)/) || L.match(/(\.wikipedia\.).+\/wiki\/([^\/]+)/) || L.match(/(youtube\.).+search_query=([^&]+)/) || L.match(/(\?.*\b)q=([^&]+)/) || L.match(/(\#search\/)([^\/]+)/)) t = decodeURIComponent(RegExp.$2);
-	}
-	Es = [[`Amazon`, `http://www.amazon.co.jp/exec/obidos/external-search/?keyword=%%`]];
-	var SP = bmlPanel(`sp`, 200);
-	SP.header.innerHTML = `検索パネル`;
-	var C = SP.content, qt, tc = [], td = [], t, i, sbm, tmp;
-	C.appendChild(qt = D.createElement(`input`));
-	qt.value = t;
-	qt.style.cssText = ` display:block;
+
+	var panel = createPanel(`hoge`, 200);
+	panel.header.innerHTML = `検索パネル`;
+	var content = panel.content, tags_input, sbm_button;
+	content.appendChild(tags_input = document.createElement(`input`));
+	tags_input.style.cssText = ` display:block;
 	width:90%;
 	margin:6px;
 	border:1px solid #666;
@@ -106,74 +106,44 @@ javascript: (function () {
 	color:inherit;
 	font-weight:bold;
 	font-size:13px;`;
-	for (i = 0; i < Es.length; i++) {
-		C.appendChild(tc[i] = D.createElement(`input`));
-		C.appendChild(td[i] = D.createElement(`div`));
-		C.appendChild(tmp = D.createElement(`div`));
-		td[i].url = Es[i][1];
-		tmp.style.cssText = `clear:both`;
-		with (tc[i]) {
-			type = `checkbox`;
-			name = i;
-			value = Es[i][1];
-			style.cssText = ` float:left;
-			clear:left;
-			display:block;
-			margin:2px 6px;`;
-		}
-		with (td[i]) {
-			style.cssText = ` cursor:pointer;`;
-			onmouseover = function () {
-				with (this.style) {
-					fontWeight = `bold`;
-					color = `#ff8`
-				}
-			}
-				;
-			onmouseout = function () {
-				with (this.style) {
-					fontWeight = `inherit`;
-					color = `inherit`
-				}
-			}
-				;
-			onclick = function () {
-				open(this.url.replace(/%%/, qt.value), `_blank`);
-			}
-				;
-			innerHTML = Es[i][0];
-		}
-	}
-	C.appendChild(sbm = D.createElement(`button`));
-	with (sbm) {
-		innerHTML = `チェックしたサービスで一括検索`;
+
+
+	content.appendChild(sbm_button = document.createElement(`button`));
+	with (sbm_button) {
+		innerHTML = `Scrapbox へ保存`;
 		style.cssText = ` cursor:pointer;
 		margin:6px auto;
 		display:block;
 		clear:both;`;
 		onclick = function () {
-            var p = document.getElementById("productTitle");
-            if (!p) var p = document.getElementById("ebooksProductTitle");
-            var title = window.prompt(`Scrap "Amazon" to your scrapbox.`, p.innerHTML);
-            if (!title) return;
-            title = `『` + title + `』`;
-            var imagecontainer = document.getElementById("imageBlockContainer");
-            if (!imagecontainer) var imagecontainer = document.getElementById("ebooksImageBlockContainer");
-            var images = imagecontainer.getElementsByTagName("img");
-            var image = images[images.length-1];
-            var imageurl = image.getAttribute("src");
-            var pub = [];
-            var c = document.getElementsByClassName(`author`);
-            for (g = 0; g < c.length; g++) {
-                let at = c[g].innerText.replace(/,/, ``);
-                let pu = at.match(/\(.+\)/);
-                let ct = at.replace(/\(.+\)/, ``).replace(/ /g, ``);
-                pub.push(pu + ` [` + ct + `]`);
-            }
-            var lines = `[` + imageurl + ` ` + window.location.href + `]\n` + pub.join(` `) + `\n#本`;
-            var body = encodeURIComponent(lines);
-            window.open(`https://scrapbox.io/oginos-reading-record/` + encodeURIComponent(title.trim()) + `?body=` + body)
-        }
+			try{			
+				var title_elm = document.getElementById("productTitle");
+				if (!title_elm) var title_elm = document.getElementById("ebooksProductTitle");
+				var title = window.prompt(`Scrap "Amazon" to your scrapbox.`, title_elm.innerHTML);
+				if (!title) return;
+
+				var images_elm = document.getElementById("imageBlockContainer");
+				if (!images_elm) var images_elm = document.getElementById("ebooksImageBlockContainer");
+				var images = images_elm.getElementsByTagName("img");
+				var image = images[images.length - 1];
+				var image_url = image.getAttribute("src");
+
+				var pub = [];
+				var authors_elm = document.getElementsByClassName(`author`);
+				for (g = 0; g < authors_elm.length; g++) {
+					let at = authors_elm[g].innerText.replace(/,/, ``);
+					let pu = at.match(/\(.+\)/);
+					let ct = at.replace(/\(.+\)/, ``).replace(/ /g, ``);
+					pub.push(pu + ` [` + ct + `]`);
+				}
+
+				var lines = `[` + image_url + ` ` + window.location.href + `]\n` + pub.join(` `) + `\n#本`;
+				var body = encodeURIComponent(lines);
+				window.open(`https://scrapbox.io/oginos-reading-record/` + encodeURIComponent(title.trim()) + `?body=` + body)
+			} catch (error){
+				alert(error);
+			}
+		}
 	}
 }
 )()
